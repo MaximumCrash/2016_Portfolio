fd4d18d645880c6ed032625af54ef5fb18d3788e
MaximumCrash <maximumcrash@hotmail.com>
2016-07-04 12:47:52 -0400

    Working on Dragging Menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
+var pageType = ["portfolio", "about", "case"];
+
+
 $(window).bind('load', function() {
 
 });
@@ -5,3 +8,18 @@ $(window).bind('load', function() {
 $(document).ready(function () {
 
 })
+
+var $portfolio = $('#portfolio');
+
+var element = document.getElementById('portfolio');
+window.mySwipe = new Swipe(element, {
+  startSlide: 0,
+  auto: 3000,
+  draggable: true,
+  autoRestart: false,
+  continuous: false,
+  disableScroll: true,
+  stopPropagation: true,
+  callback: function(index, element) {},
+  transitionEnd: function(index, element) {}
+});
