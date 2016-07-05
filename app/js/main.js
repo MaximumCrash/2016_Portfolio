1987f19b5cf110a88c7eab614f3928c7b3a37197
MaximumCrash <maximumcrash@hotmail.com>
2016-07-04 22:44:29 -0400

    Scroll Menu Implemented

--- app/js/main.js
+++ app/js/main.js
@@ -11,15 +11,12 @@ $(document).ready(function () {
 
 var $portfolio = $('#portfolio');
 
-var element = document.getElementById('portfolio');
-window.mySwipe = new Swipe(element, {
-  startSlide: 0,
-  auto: 3000,
-  draggable: true,
-  autoRestart: false,
-  continuous: false,
-  disableScroll: true,
-  stopPropagation: true,
-  callback: function(index, element) {},
-  transitionEnd: function(index, element) {}
-});
+
+$portfolio.flickity({
+  cellAlign:'left',
+  contain: true,
+  freeScroll: true,
+  prevNextButtons: false,
+pageDots: false,
+imagesLoaded: true
+})
