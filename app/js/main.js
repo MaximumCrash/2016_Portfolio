6d54363272e5a19a7eda1768cf62889c823c1c24
MaximumCrash <maximumcrash@hotmail.com>
2016-07-05 08:25:01 -0400

    Adding Sorting WIP

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,6 @@ $(window).bind('load', function() {
 
 });
 
-$(document).ready(function () {
-
-})
 
 var $portfolio = $('#portfolio');
 
@@ -19,4 +16,64 @@ $portfolio.flickity({
   prevNextButtons: false,
 pageDots: false,
 imagesLoaded: true
+})
+
+var Shuffle = window.shuffle;
+var element = document.getElementById('portfolio');
+var $filterOptions = $('.secondary > ul > li')
+
+
+$(document).ready(function () {
+  var shuffle = new Shuffle(element, {
+    itemSelector: '.carousel-cell'
+  })
+
+  $filterOptions.children().on('click', function () {
+
+
+    $this = $(this);
+
+    if (!$this.hasClass('active')){
+    console.log($this)
+    var sort = $this.attr('data-sort'),
+        opts = {};
+        console.log(sort)
+        if (sort === 'games') {
+          opts = {
+            by: function($el) {
+              return el.data('games')
+            }
+          }
+        }
+          for (var k =0; k < $filterOptions.children().length; k++) {
+            var which = $filterOptions.children()[k];
+            var $which = $(which);
+            if ($which.hasClass('active')) {
+              $which.removeClass('active').addClass('inactive');
+              break;
+            }
+          }
+
+          $this.removeClass('inactive').addClass('active');
+        shuffle.sort( opts);
+      }
+  })
+  var setupFilters = function () {
+    var $btns = $filterOptions.children();
+    $btns.on('click', function () {
+      console.log("Hello")
+      var $this = $(this),
+      isActive = $this.hasClass('active'),
+      group =isActive ? 'all' : $this.data('group');
+
+      if (!isActive) {
+        $('.secondary > ul >li > .active').removeClass('active');
+      }
+        $this.toggleClass('active');
+
+        $('#portfolio').shuffle('shuffle', group);
+
+    });
+    $btns = null;
+  }
 })
